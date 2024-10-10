1d5568b3ed7c30cd0d44c73712089696d810ce58
Mwangi <83245657+alexmoshte@users.noreply.github.com>
2024-10-10 16:21:44 +0300

    Update _ADCn_INx_AR.h

--- _ADCn_INx_AR.h
+++ _ADCn_INx_AR.h
@@ -20,16 +20,9 @@
 //FUNCTION DECLARATION
 void ADC1_IN1_autocorr_calc(void); // Calculates the auto-correlation between data and generates auto-correlation values that are stored in a buffer
 void ADC1_IN2_autocorr_calc(void);
-void ADC2_IN3_autocorr_calc(void);
-void ADC2_IN4_autocorr_calc(void);
-void ADC3_IN1_autocorr_calc(void);
-void ADC3_IN2_autocorr_calc(void);
+
 
 float32_t* ADC1_IN1_autoreg_coeffs(void); // Calculates the auto-regression coefficients using the Yule-Walker equations. The coefficients are stored in a buffer
 float32_t* ADC1_IN2_autoreg_coeffs(void);
-float32_t* ADC2_IN3_autoreg_coeffs(void);
-float32_t* ADC2_IN4_autoreg_coeffs(void);
-float32_t* ADC3_IN1_autoreg_coeffs(void);
-float32_t* ADC3_IN2_autoreg_coeffs(void);
 
 #endif /* INC__ADCN_INX_AR_H_ */
